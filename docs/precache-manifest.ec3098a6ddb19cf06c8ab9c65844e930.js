7f749760b9b6a5e29836936e3cb36bdcf9a210a2
David Barcenas <daveepro@outlook.com>
2020-06-27 13:21:14 -0500

    feat: Add docs folder for deployment

---
+++ docs/precache-manifest.ec3098a6ddb19cf06c8ab9c65844e930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf0674d069b7e34fdcc2051e6d1c622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38f5dfe72e2282d73d02",
+    "url": "/static/css/main.a1b77d25.chunk.css"
+  },
+  {
+    "revision": "9e7f6b33791e72c22c96",
+    "url": "/static/js/2.c2365477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2365477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f5dfe72e2282d73d02",
+    "url": "/static/js/main.abc22354.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
